4ef82002d664918c1932e47f4940c1a2b778e7ce
shigT <11024306@mmm.muroran-it.ac.jp>
2015-09-29 12:22:14 +0900

    Add stuffs for chrono::engine.

---
+++ src/chrono/KalChStuff.cpp
@@ -0,0 +1,13 @@
+/**************************************************************/
+/*                                                            */
+/*     ================[ KaLMIA Library ]================     */
+/*                                                            */
+/*           Kaji-Lab Moderate Instrumental Assets            */
+/*                                                            */
+/*     ============== " Be Ambitious For " ==============     */
+/*                                                            */
+/*              Muroran Institute of Technology               */
+/*      Electronic System Control Engineering Laboratory      */
+/*                                                            */
+/**************************************************************/
+
