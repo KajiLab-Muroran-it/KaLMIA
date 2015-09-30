06e01f9e75677d79cb4a73c7c5cae780fcb79bc9
shigT <11024306@mmm.muroran-it.ac.jp>
2015-09-30 10:13:39 +0900

    Slight code refactoring of KalChStuff.

--- src/chrono/KalChStuff.hpp
+++ src/chrono/KalChStuff.hpp
@@ -25,26 +25,27 @@ namespace kalmia {
 namespace chrono {
 	// Sphere
 	template <class T>
-	T MassToDens (T mass, T radius){
+	inline T MassToDens (T mass, T radius){
 		return mass / (4. / 3. * ::chrono::CH_C_PI * radius * radius * radius);
 	}
-	//
+	// Cylinder
 	template <class T>
-	T MassToDens (T mass, T radius, T height){
+	inline T MassToDens (T mass, T radius, T height){
 		return mass / (::chrono::CH_C_PI * radius * radius * height);
 	}
+	// Box
 	template <class T>
-	T MassToDens (T mass, T depth, T height, T width){
+	inline T MassToDens (T mass, T depth, T height, T width){
 		return mass / (depth * height * width);
 	}
 
 	double GetGravitionalPotential (::chrono::ChSharedPtr<::chrono::ChBody> body, ::chrono::ChVector<> G_acc){
-		return body->GetMass () * G_acc.Dot (body->GetPos ());
+		return -1.* body->GetMass () * G_acc.Dot (body->GetPos ());
 	}
 
 	double GetKineticEnergy (::chrono::ChSharedPtr<::chrono::ChBody> body){
-		::chrono::ChVector<> vel = body->GetPos_dt();
-		::chrono::ChVector<> wvel = body->GetWvel_loc();
+		const ::chrono::ChVector<> vel = body->GetPos_dt();
+		const ::chrono::ChVector<> wvel = body->GetWvel_loc();
 		return (body->GetMass() * vel.Dot (vel) + wvel.Dot (body->GetInertia() * wvel)) / 2.;
 	}
 }
