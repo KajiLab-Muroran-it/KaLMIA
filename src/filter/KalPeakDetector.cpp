c2a622fce85a622478ae37e7bbbc0e2b1d320143
shigT <11024306@mmm.muroran-it.ac.jp>
2015-10-09 13:21:15 +0900

    Fixed: the peak detector had problem with offset value. Now it works well.

--- src/filter/KalPeakDetector.cpp
+++ src/filter/KalPeakDetector.cpp
@@ -19,23 +19,20 @@ namespace filter{
 
 void KalPeakDetector::Update_impl (double t, double process_value){
 	on_peak_ = false;
-	if ((abs (process_value) < abs (prev_value_)) && ((prev_value_ * peak_amplitude_) <= 0)){
-		if ((process_value < prev_value_ && detect_peak_) || (process_value > prev_value_ && detect_bottom_)){
+
+	bool slope = (process_value - prev_value_) > 0.;
+
+	if (slope != prev_slope_){
+		if ((detect_peak_ && !slope) || (detect_bottom_ && slope)){
 			on_peak_ = true;
+			peak_amplitude_ = prev_value_;
 		}
-		prev_peak_amplitude_ = peak_amplitude_;
-		peak_amplitude_ = prev_value_;
 	}
+
+	prev_slope_ = slope;
 	prev_value_ = process_value;
 }
 
-double KalPeakDetector::Output_impl () {
-	if ((peak_amplitude_ >= 0 && detect_peak_) || (peak_amplitude_ < 0 && detect_bottom_)){
-		return peak_amplitude_;
-	} else{
-		return prev_peak_amplitude_;
-	}
-};
 
 }
 }
--- src/filter/KalPeakDetector.hpp
+++ src/filter/KalPeakDetector.hpp
@@ -23,32 +23,32 @@ namespace filter{
 
 class KalPeakDetector : public KalFilterBase{
 public:
-	KalPeakDetector (bool detect_peak = true, bool detect_bottom = false) 
-		: detect_peak_(detect_peak)
-		, detect_bottom_(detect_bottom)
+	KalPeakDetector (bool detect_peak = true, bool detect_bottom = false)
+		: detect_peak_ (detect_peak)
+		, detect_bottom_ (detect_bottom)
 		, on_peak_ (false)
-		, peak_amplitude_ (0.)
-		, prev_peak_amplitude_ (0.)
-		, prev_value_ (0.) {};
+		, prev_slope_ (true)
+		, peak_amplitude_ (0.) {}
+
 	virtual ~KalPeakDetector() = default;
 
 	bool OnPeak () { return on_peak_; };
 
 private:
 	virtual void Update_impl (double t, double process_value) override;
-	virtual double Output_impl () override;
+	virtual double Output_impl () override { return peak_amplitude_; };
 
 	const bool detect_peak_;
 	const bool detect_bottom_;
+
 	bool on_peak_;
+	bool prev_slope_; // true for positive slope
 	double peak_amplitude_;
-	double prev_peak_amplitude_;
 	double prev_value_;
 };
 
-
-}
-}
+} // namespace filter
+} // namespace kalmia
 
 #ifdef KALMIA_HEADER_ONLY
 #include "KalPeakDetector.cpp"
